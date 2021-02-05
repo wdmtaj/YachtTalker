2228764ddc286553460b6defe9d5a98e4f01f90a
William Miller <bill@millermailbox.com>
2021-02-04 21:45:25 -0500

    Initial Commit

--- YachtTalker.py
+++ YachtTalker.py
@@ -7,7 +7,7 @@ import json
 from time import *
 from tkinter import *
 import threading
-import pyttsx3
+# import pyttsx3
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
 # Custom MQTT message callback
@@ -23,7 +23,7 @@ host = "a39fvf5mz5tuzf-ats.iot.us-east-1.amazonaws.com"     # this is the endpoi
 rootCAPath = "X509_Root.crt"                                # Created from AWS IoT Certificate Creation with Policy
 certificatePath = "209c3a4c4a-certificate.pem.crt"          # Created from AWS IoT Certificate Creation with Policy
 privateKeyPath = "209c3a4c4a-private.pem.key"               # Created from AWS IoT Certificate Creation with Policy
-                                                            # Pilocy attached to above certificate allows all
+                                                            # Policy attached to above certificate allows all
                                                             #   IoT actions and resources
                                                             # {
                                                             #   "Version": "2012-10-17",
@@ -62,80 +62,22 @@ sleep(2)
 
 false = False
 
-# Alexa JSON data structure with additions for CAN bus data at end
-alexa_message = {
-    "version": "1.0",
-    "session": {
-        "new": false,
-        "sessionId": "OA7821",
-        "application": {
-            "applicationId": ""
-        },
-        "user": {
-            "userId": ""
-        }
-    },
-    "context": {
-        "Extensions": {
-            "available": {}
-        },
-        "System": {
-            "application": {
-                "applicationId": ""
-            },
-            "user": {
-                "userId": ""
-            },
-            "device": {
-                "deviceId": "",
-                "supportedInterfaces": {}
-            },
-            "apiEndpoint": "",
-            "apiAccessToken": "",
-            "unit": {
-                "unitId": ""
-            }
-        }
-    },
-    "request": {
-        "type": "OA7821_Data_Update",
-        "requestId": "",
-        "timestamp": "",
-        "locale": "",
-        "shouldLinkResultBeReturned": false
-    },
-    "can_bus_data": {
-        "fresh_water_level": "0",
-        "heading_magnetic": "",
-        "black_water_level": "",
-        "lattitude": "",
-        "longitude": "",
-        "fuel_tank_level": ""
-    }
-}
-
 shadow_update_msg = {
-  "desired": {
-    "welcome": "OA7821 Shadow Update"
-  },
-  "reported": {
-    "welcome": "OA7821 Shadow Update"
-  },
-    "can_bus_data": {
-        "fresh_water_level": "0",
-        "heading_magnetic": "",
-        "black_water_level": "",
-        "lattitude": "",
-        "longitude": "",
-        "fuel_tank_level": ""
-    },
-}
+    "state":
+            {
+      "reported": {
+                "can_bus_data": {
+                            "fresh_water_level": "358",
+                            "heading_magnetic": "090",
+                            "black_water_level": "18",
+                            "lattitude": "",
+                            "longitude": "",
+                            "fuel_tank_level": ""
+                                }
+                }
+            }
+    }
 
-#  Start CAN Bus interface and configure
-os.system('sudo ifconfig can0 down')
-os.system('sudo ip link set can0 up type can bitrate 250000')
-os.system('sudo ifconfig can0 up')
-can0 = can.interface.Bus(channel='can0', bustype='socketcan_ctypes')
 
 def get_can_data():  # function runs in background, reads and converts CAN bus data
 
@@ -221,26 +163,39 @@ def start_bg_canbus():
     can_thread = threading.Thread(target=get_can_data, daemon=True)
     can_thread.start()
 
+def send_CAN_msg():
+    heading_degrees = 180
+    shadow_update_msg['state']['reported']['can_bus_data']['heading_magnetic'] = str(heading_degrees)
+    shadow_msg_Json = json.dumps(shadow_update_msg)
+    myAWSIoTMQTTClient.publish("$aws/things/Shadow_Test_Thing/shadow/update", shadow_msg_Json, 1)
+    print(shadow_msg_Json)
+    return None
 
 def turn_off_can_bus():
     os.system('sudo ifconfig can0 down')
 
 
 def turn_on_can_bus():
+    #  Start CAN Bus interface and configure
+    os.system('sudo ifconfig can0 down')
+    os.system('sudo ip link set can0 up type can bitrate 250000')
     os.system('sudo ifconfig can0 up')
+    can0 = can.interface.Bus(channel='can0', bustype='socketcan_ctypes')
 
     # These statements build the GUI interface
 
 
-l2 = Label(win, text=' ', font='Times 24 bold')
+l2 = Label(win, text=' ', font='Times 12 bold')
 l2.pack()
-b4 = Button(win, text='Start Background CAN bus reader', font='Times 24 bold', command=start_bg_canbus)
+b4 = Button(win, text='Start Background CAN bus reader', font='Times 12 bold', command=start_bg_canbus)
 b4.pack()
-b_can_off = Button(win, text='Turn CAN Bus Off', font='Times 24 bold', command=turn_off_can_bus)
+b_send_can_msg = Button(win, text='Send single CAN msg', font='Times 12 bold', command=send_CAN_msg)
+b_send_can_msg.pack()
+b_can_off = Button(win, text='Turn CAN Bus Off', font='Times 12 bold', command=turn_off_can_bus)
 b_can_off.pack()
-b_can_on = Button(win, text='Turn CAN Bus On', font='Times 24 bold', command=turn_on_can_bus)
+b_can_on = Button(win, text='Turn CAN Bus On', font='Times 12 bold', command=turn_on_can_bus)
 b_can_on.pack()
-b_quit = Button(win, text='quit', font='Times 24 bold', command=win.quit)
+b_quit = Button(win, text='quit', font='Times 12 bold', command=win.quit)
 b_quit.pack()
 
 win.mainloop()  # main GUI window loop
